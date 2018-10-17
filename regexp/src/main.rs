8be371beee06512a8361f7b1ce0340bdfec7ac9e
wachel@163.com <wachel@163.com>
2018-10-17 14:29:39 +0800

    aaaa

--- regexp/src/main.rs
+++ regexp/src/main.rs
@@ -1,8 +1,22 @@
+enum Op{
+    Or,
+    FromTo(char,char),
+    Link,
+}
 
+struct Node{
+    v:Op,
+    c0:Box<Node>,
+    c1:Box<Node>
+}
+
+fn parse(r:&str){
+    
+}
 
 
 fn main() {
     let s0 = "int a = 3;";
-    let s1 = "if( a >= 0){}"
+    let s1 = "if( a >= 0){}";
     println!("Hello, world!");
 }
