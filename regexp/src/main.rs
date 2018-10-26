45fa113d97e50de9b0a077787ac425567d5c0322
wachel <wachel@163.com>
2018-10-26 21:07:41 +0800

    aaa

--- regexp/src/main.rs
+++ regexp/src/main.rs
@@ -6,6 +6,7 @@ use std::fmt;
 //    c1:Option<Box<Node>>
 //}
 
+#[derive(PartialEq)]
 enum Node{
     Char(u8),
     FromTo(u8,u8),
@@ -13,9 +14,10 @@ enum Node{
     Or(Box<Node>,Box<Node>),
     Link(Box<Node>,Box<Node>),
     None,
+    Error,
 }
 
-#[derive(Debug)]
+#[derive(Debug,PartialEq)]
 enum Token{
     Char(u8),
     FromTo(u8,u8),//-
@@ -91,29 +93,95 @@ fn to_ast(tokens:&[Token], node:Node)->Node{
     }
 }
 */
-
-fn to_ast(stack:&Vec<Token>,tokens:&Vec<Token>)->Node{
-    let mut index = 0;
-    match tokens[index] {
-        Token::Char(c)=>Node::Char(c),
-        Token::FromTo(c0,c1)=>Node::FromTo(c0,c1),
-        Token::Repeat=>Node::Repeat(Box::new(node)),
-        Token::Or=>Node::Or(Box::new(node),Box::new(to_ast(&tokens[1..],Node::None))),
-        Token::Split(c)=>{
-            if c == b'['{
-                
-                to_ast(&tokens[1..],Node::None)
+fn parse_factor(tokens:&[Token])->(Node,usize){
+    match tokens[0] {
+        Token::Char(c) => {
+            let node:Node = Node::Char(c);
+            return (node,1);
+        },
+        Token::Split(c) => {
+            if c == b'('{
+                let (node,length) = parse_expression(&tokens[1..]);
+                if node != Node::None && length > 0{
+                    return (node,length);
+                }
             }
-        }
+        },
+        _=>{}
     }
-    Node::None
+    (Node::Error,0)
 }
 
+fn parse_term(tokens:&[Token])->(Node,usize){
+    let mut index:usize = 0;
+    let (node0,length0) = parse_factor(tokens);
+    if node0 != Node::None && length0 > 0{
+        index += length0;
+        if index <= tokens.len(){
+            if tokens[index] == Token::Repeat {
+                let node:Node = Node::Repeat(Box::new(node0));
+                return (node,index);
+            }
+            else{
+                let (node1,length1) = parse_term(&tokens[index..]);
+                if node1 != Node::None && length1 > 0 {
+                    index += length1;
+                    let node:Node = Node::Link(Box::new(node0),Box::new(node1));
+                    return (node,index);
+                }
+            }
+        }
+        else{
+            return (node0,index);
+        }
+    }
+
+    (Node::Error,0)
+}
+
+fn parse_expression(tokens:&[Token])->(Node,usize){
+    let mut index:usize = 0;
+    let (node0,length0) = parse_term(tokens);
+    if node0 != Node::None && length0 > 0{
+        index += length0;
+        if index <= tokens.len(){
+            if tokens[index] == Token::Or {
+                let (node1,length1) = parse_expression(&tokens[index..]);
+                if node1 != Node::None && length1 > 0 {
+                    index += length1;
+                    let node:Node = Node::Or(Box::new(node0),Box::new(node1));
+                    return (node,index);
+                }
+            }
+        }
+        else{
+            return (node0,index);
+        }
+    }
+
+    (Node::Error,0)
+}
+
+// fn to_ast(tokens:&Vec<Token>)->Node{
+//     let mut index = 0;
+//     match tokens[index] {
+//         Token::Char(c)=>Node::Char(c),
+//         Token::FromTo(c0,c1)=>Node::FromTo(c0,c1),
+//         Token::Repeat=>Node::Repeat(Box::new(node)),
+//         Token::Or=>Node::Or(Box::new(node),Box::new(to_ast(&tokens[1..],Node::None))),
+//         Token::Split(c)=>{
+//             if c == b'['{
+//                 stack.push(tokens[index]);
+//                 to_ast(&tokens[1..],Node::None)
+//             }
+//         }
+//     }
+//     Node::None
+// }
+
 fn main() {
-    let tokens:Vec<Token> = parse_tokens("[a-z]*");
-    let mut tempStack:Vec<Token> = Vec::new(); 
-    
-    let node:Node = to_ast(tokens.as_slice(),Node::None);
+    let tokens:Vec<Token> = parse_tokens("a");
+    let (node,length) = parse_expression(&tokens[0..]);
     for t in tokens{
         println!("{}",t);
     }
